ca7cfd859144f4bfead3fdcf97e27f71a3dc0cf8
TANJX <327060609@qq.com>
2016-01-17 21:30:58 +0800

    Add Config

--- src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/SubtleRPG.java
+++ src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/SubtleRPG.java
@@ -1,10 +1,17 @@
-/**
+ï»¿/**
  * 
  */
 package cc.isotopestudio.SubtleRPG.subtlerpg;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,30 +21,77 @@ import cc.isotopestudio.SubtleRPG.subtlerpg.listener.*;
  * @author Mars
  *
  */
-public final class SubtleRPG extends JavaPlugin {
+public final class SubtleRPG extends JavaPlugin implements Listener {
+
+	public final String prefix = (new StringBuilder()).append(ChatColor.GREEN).append("[").append(ChatColor.ITALIC)
+			.append(ChatColor.BOLD).append("SubtleRPG").append(ChatColor.RESET).append(ChatColor.GREEN).append("]")
+			.append(ChatColor.RESET).toString();
+
+	public void createFile(String name) {
+		File file;
+		file = new File(getDataFolder(), name + ".yml");
+		if (!file.exists()) {
+			getConfig().options().copyDefaults(true);
+			try {
+				getConfig().save(file);
+			} catch (IOException ex) {
+				getLogger().severe("é…ç½®æ–‡ä»¶ä¿å­˜å‡ºé”™");
+				getServer().getPluginManager().disablePlugin(this);
+				return;
+			}
+		}
+	}
+
 	@Override
 	public void onEnable() {
+		getLogger().info("åŠ è½½é…ç½®æ–‡ä»¶ä¸­");
+		createFile("config");
+		createFile("groups");
+		createFile("playersData");
 
-		 PluginManager pm = this.getServer().getPluginManager();
-		 pm.registerEvents(new TestListener(this), this);
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvents(new TestListener(this), this);
 
-		getLogger().info("SubtleRPG ³É¹¦¼ÓÔØ!");
+		getLogger().info("SubtleRPG æˆåŠŸåŠ è½½!");
+	}
+
+	public void onReload() {
+		this.reloadConfig();
 	}
 
 	@Override
 	public void onDisable() {
-		getLogger().info("SubtleRPG ³É¹¦Ğ¶ÔØ!");
+		getLogger().info("SubtleRPG æˆåŠŸå¸è½½!");
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		if (cmd.getName().equalsIgnoreCase("SubtleRPG")) { // If the player typed /basic then do the following...
-			// doSomething
-			getLogger().info("Íæ¼ÒÊäÈëÃüÁî");
-			sender.sendMessage("red.");
-			return true;
-		}
-		return false; 
+		if (cmd.getName().equalsIgnoreCase("SubtleRPG"))
+			if (args.length > 0 && !args[0].equals("help")) {
+
+				if (args[0].equals("health") && args.length == 5) {
+
+					Player player = Bukkit.getPlayer(args[1]);
+					double newHealth = (double) Integer.parseInt(args[2]);
+					double newMaxHealth = (double) Integer.parseInt(args[3]);
+					double newHealthScale = (double) Integer.parseInt(args[4]);
+
+					player.setHealth(newHealth);
+					player.setMaxHealth(newMaxHealth);
+					player.setHealthScale(newHealthScale);
+
+					return true;
+				} else {
+					sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.RED).append("æœªçŸ¥å‘½ä»¤").toString());
+					return true;
+				}
+			} else {
+				sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.AQUA).append("å¸®åŠ©èœå•").toString());
+
+				return true;
+			}
+
+		return false;
 	}
 
 }
