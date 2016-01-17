48e5f907314396684262b4f04daa5b4537ebf570
TANJX <327060609@qq.com>
2016-01-17 23:21:26 +0800

    Add EntityDamgeEvent

--- src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/listener/TestListener.java
+++ src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/listener/TestListener.java
@@ -1,10 +1,12 @@
-package cc.isotopestudio.SubtleRPG.subtlerpg.listener;
+﻿package cc.isotopestudio.SubtleRPG.subtlerpg.listener;
 
-import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 import cc.isotopestudio.SubtleRPG.subtlerpg.SubtleRPG;
 
@@ -14,17 +16,24 @@ public class TestListener implements Listener {
 	public TestListener(SubtleRPG instance) {
 		plugin = instance;
 	}
-	
-	@EventHandler
-	public void onPlayerMove(PlayerMoveEvent event) {
-		Player player = event.getPlayer();
-		Location playerLoc = player.getLocation();
-		player.sendMessage("Your X Coordinates : " + playerLoc.getX());
-		player.sendMessage("Your Y Coordinates : " + playerLoc.getY());
-		player.sendMessage("Your Z Coordinates : " + playerLoc.getZ());
 
+	@EventHandler
+	public void onPlayerLogin(PlayerJoinEvent event) {
+		Player player = event.getPlayer();
+		player.sendMessage(plugin.prefix + player.getName() + "登陆");
+		double health = plugin.getConfig().getInt((new StringBuilder("Health.Scale")).toString());
+		player.setHealthScale(health);
 	}
-	
-	
+
+	@EventHandler
+	public void onDamage(EntityDamageByEntityEvent event) {
+		Entity damager = event.getDamager();
+		Entity damagee = event.getEntity();
+		if (damager instanceof Player && damagee instanceof LivingEntity) {
+			Player player = (Player) damager;
+			player.sendMessage(player.getName() + "攻击了" + damagee.getType());
+			event.setDamage(100.0);
+		}
+	}
 
 }
