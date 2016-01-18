1996c2e05e00a927c6d99d25b4fa415d78d09a29
TANJX <327060609@qq.com>
2016-01-18 10:28:06 +0800

    Add AttackSystem

--- src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/SubtleRPG.java
+++ src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/SubtleRPG.java
@@ -5,11 +5,14 @@ package cc.isotopestudio.SubtleRPG.subtlerpg;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
@@ -22,6 +25,8 @@ import cc.isotopestudio.SubtleRPG.subtlerpg.listener.*;
  *
  */
 public final class SubtleRPG extends JavaPlugin implements Listener {
+
+	public FileConfiguration groupsFile;
 
 	public final String prefix = (new StringBuilder()).append(ChatColor.GREEN).append("[").append(ChatColor.ITALIC)
 			.append(ChatColor.BOLD).append("SubtleRPG").append(ChatColor.RESET).append(ChatColor.GREEN).append("]")
@@ -45,9 +50,12 @@ public final class SubtleRPG extends JavaPlugin implements Listener {
 	@Override
 	public void onEnable() {
 		getLogger().info("加载配置文件中");
-		createFile("config");
+		saveDefaultConfig();
+		// createFile("config");
 		createFile("groups");
-		createFile("playersData");
+		groupsFile = YamlConfiguration.loadConfiguration(getResource("groups.yml"));
+
+		// createFile("playersData");
 
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvents(new TestListener(this), this);
@@ -69,6 +77,30 @@ public final class SubtleRPG extends JavaPlugin implements Listener {
 		if (cmd.getName().equalsIgnoreCase("SubtleRPG"))
 			if (args.length > 0 && !args[0].equals("help")) {
 
+				if (args[0].equals("info")) {
+					if ((sender instanceof Player && (args.length == 1 || args.length == 2))
+							|| (!(sender instanceof Player) && args.length == 2)) {
+						if (args.length == 2) {
+							Player player = (Bukkit.getServer().getPlayer(args[1]));
+							if (player == null) {
+								sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.RED).append("玩家")
+										.append(args[1]).append("不存在").toString());
+								return true;
+							} else {
+								sendInfo(sender, args, player);
+								return true;
+							}
+						} else {
+							sendInfo(sender, args, (Player) sender);
+							return true;
+						}
+
+					} else {
+						sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.RED)
+								.append("/subtleRPG info <玩家名字>").toString());
+					}
+				}
+
 				if (args[0].equals("health") && args.length == 5) {
 
 					Player player = Bukkit.getPlayer(args[1]);
@@ -85,13 +117,47 @@ public final class SubtleRPG extends JavaPlugin implements Listener {
 					sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.RED).append("未知命令").toString());
 					return true;
 				}
-			} else {
+			} else
+
+			{
 				sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.AQUA).append("帮助菜单").toString());
 
 				return true;
 			}
 
 		return false;
+	}
+
+	public void sendInfo(CommandSender sender, String[] args, Player player) {
+		sender.sendMessage((new StringBuilder(prefix)).append(ChatColor.AQUA).append("玩家").append(player.getName())
+				.append("信息").toString());
+		List<String> list = getConfig().getStringList("Groups");
+		double addDamage = 0;
+		for (int i = 0; i < list.size(); i++) {
+			String tempPermission = "subtleRPG." + list.get(i);
+			if (player.hasPermission(tempPermission)) {
+				sender.sendMessage((new StringBuilder()).append(ChatColor.DARK_GREEN).append("职业: ")
+						.append(groupsFile.getString(list.get(i) + ".name")).toString());
+				addDamage = groupsFile.getDouble(list.get(i) + ".Attack.default")
+						+ groupsFile.getDouble(list.get(i) + ".Attack.increasePerPeriod")
+								* (int) (player.getLevel() / groupsFile.getDouble(list.get(i) + ".Attack.levPeriod"));
+				if (addDamage > groupsFile.getDouble(list.get(i) + ".Attack.max")) {
+					addDamage = groupsFile.getDouble(list.get(i) + ".Attack.max");
+				}
+				break;
+
+			}
+			if (i >= list.size() - 1) {
+				sender.sendMessage((new StringBuilder()).append(ChatColor.DARK_GREEN).append("ta没有职业").toString());
+			}
+		}
+
+		sender.sendMessage((new StringBuilder()).append(ChatColor.DARK_RED).append("HP: ").append(player.getHealth())
+				.append(" / ").append(player.getMaxHealth()).toString());
+
+		sender.sendMessage(
+				(new StringBuilder()).append(ChatColor.YELLOW).append("攻击加成: ").append(addDamage).toString());
+
 	}
 
 }
--- src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/listener/TestListener.java
+++ src/main/java/cc/isotopestudio/SubtleRPG/subtlerpg/listener/TestListener.java
@@ -1,5 +1,7 @@
 ﻿package cc.isotopestudio.SubtleRPG.subtlerpg.listener;
 
+import java.util.List;
+
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -31,8 +33,45 @@ public class TestListener implements Listener {
 		Entity damagee = event.getEntity();
 		if (damager instanceof Player && damagee instanceof LivingEntity) {
 			Player player = (Player) damager;
-			player.sendMessage(player.getName() + "攻击了" + damagee.getType());
-			event.setDamage(100.0);
+			List<String> list = plugin.getConfig().getStringList("Groups");
+			double addDamage = 0, addDefence = 0;
+			for (int i = 0; i < list.size(); i++) {
+				String tempPermission = "subtleRPG." + list.get(i);
+				if (player.hasPermission(tempPermission)) {
+					
+					player.sendMessage("You are in group " + plugin.groupsFile.getString(list.get(i) + ".name"));
+					player.sendMessage("default: " + plugin.groupsFile.getDouble(list.get(i) + ".Attack.default"));
+					player.sendMessage("increasePerPeriod: "
+							+ plugin.groupsFile.getDouble(list.get(i) + ".Attack.increasePerPeriod"));
+					player.sendMessage("level: " + player.getLevel());
+					player.sendMessage("levPeriod: " + plugin.groupsFile.getDouble(list.get(i) + ".Attack.levPeriod"));
+
+					addDamage = plugin.groupsFile.getDouble(list.get(i) + ".Attack.default")
+							+ plugin.groupsFile.getDouble(list.get(i) + ".Attack.increasePerPeriod")
+									* (int) (player.getLevel()
+											/ plugin.groupsFile.getDouble(list.get(i) + ".Attack.levPeriod"));
+					if (addDamage > plugin.groupsFile.getDouble(list.get(i) + ".Attack.max")) {
+						addDamage = plugin.groupsFile.getDouble(list.get(i) + ".Attack.max");
+					}
+					addDefence = 1;
+					break;
+				}
+
+				if (i >= list.size() - 1) {
+					addDamage = plugin.getConfig().getDouble("DefaultGroup.Attack.default") + plugin.getConfig()
+							.getDouble("DefaultGroup.Attack.increasePerPeriod")
+							* (int) (player.getLevel() / plugin.getConfig().getDouble("DefaultGroup.Attack.levPeriod"));
+					if (addDamage > plugin.getConfig().getDouble("DefaultGroup.Attack.max")) {
+						addDamage = plugin.getConfig().getDouble("DefaultGroup.Attack.max");
+					}
+					addDefence = 0;
+
+				}
+			}
+			double newDamage = addDamage + event.getDamage();
+			player.sendMessage(
+					player.getName() + "攻击了" + damagee.getType() + "原伤害：" + event.getDamage() + "加成伤害" + addDamage);
+			event.setDamage(newDamage);
 		}
 	}
 
